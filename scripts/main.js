4dad8dac9147989fecce07118d58744c636a7028
Barrett Cox <barrettcox80@gmail.com>
2016-07-05 16:19:35 -0700

    Updated version number in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------------
     
-  Duende Trellisto, v1.0.0
+  Duende Trellisto, v1.0.1
 
   Authors     : Barrett Cox, http://barrettcox.com
                 Amy Wu,      http://duende.us
